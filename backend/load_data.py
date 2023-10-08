589118625527964aae56d811aa46a64d97fe46c6
Your Name <anirudhm.techandscience@gmail.com>
2023-10-08 13:58:45 +0000

    add some more stuff

--- backend/load_data.py
+++ backend/load_data.py
@@ -13,6 +13,9 @@ class loadData:
         self._dirPathQuasar = qsoPath
         self._dirPathOther = othPath
         self.qsoSpectra = np.empty((0, 2, 5000), dtype=np.longdouble)
+        self.othSpectra = np.empty((0, 2, 5000), dtype=np.longdouble)
+        self.othLabels = None
+        self.qsoLabels = None
         
     def loadQSOData(self):
         try:
@@ -26,12 +29,29 @@ class loadData:
                         #TODO: Implement a dynamic maximum pad length system
                         loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant', constant_values=0)
                         flux = np.pad(flux, (0,5000 - len(flux)), mode='constant', constant_values=0)
-                        spectrum = np.stack([loglam,flux])#stacks the two flux and loglam vectors into a matrix
+                        spectrum = np.stack([loglam,flux])
                         self.qsoSpectra = np.append(self.qsoSpectra, [spectrum], axis=0)
+            self.qsoLabels = np.ones(self.qsoSpectra.shape[0])
         except FileNotFoundError:
             print("FileNotFoundError, please check if the path is valid")
         except Exception as e:
             print(f"A error occured : {e}")
-
-ld = loadData()
-ld.loadQSOData()
+    def loadOthData(self):
+        try:
+            for fileName in os.listdir(self._dirPathOther):
+                if fileName.endswith('.fits'):
+                    filePath = os.path.join(self._dirPathOther, fileName)
+                    with fs.open(filePath) as spec:
+                        binHDU = spec[1].data
+                        loglam = np.array(binHDU['loglam'],dtype=np.longdouble)
+                        flux = np.array(binHDU['flux'],dtype=np.longdouble)
+                        #TODO: Implement a dynamic maximum pad length system
+                        loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant', constant_values=0)
+                        flux = np.pad(flux, (0,5000 - len(flux)), mode='constant', constant_values=0)
+                        spectrum = np.stack([loglam,flux])
+                        self.othSpectra = np.append(self.othSpectra, [spectrum], axis=0)
+            self.othLabels = np.zeros(self.othSpectra.shape[0])
+        except FileNotFoundError:
+            print("FileNotFoundError, please check if the path is valid")
+        except Exception as e:
+            print(f"A error occured : {e}")
