2af92d64a33d1b8c398a6b43aa874b017290ae3a
Your Name <anirudhm.techandscience@gmail.com>
2023-10-09 16:34:40 +0000

    clean up the code,and for now this  file is done.
    Now ill work on:
    -The  neural  network itself
    
    -Add main.py
    
    -Add tests

--- backend/load_data.py
+++ backend/load_data.py
@@ -9,7 +9,9 @@ import os
 
 class loadData:
 
-    def __init__(self,qsoPath="/home/tux/Downloads/QSO",othPath="/home/tux/Downloads/oth"):
+    def __init__(self,qsoPath="/home/tux/Downloads/QSO",
+    othPath="/home/tux/Downloads/oth"):
+
         self._dirPathQuasar = qsoPath
         self._dirPathOther = othPath
         self._qsoSpectra = np.empty((0, 2, 5000), dtype=np.longdouble)
@@ -18,8 +20,9 @@ class loadData:
         self._qsoLabels = None
         self.labels = None
         self.spectra = None
+        self._maxLength = 5000
         
-    def loadQSOData(self):
+    def _loadQSOData(self):
         """
             This loads the relevant columns from the .fits files , which is basically 
             the spectrum, and then makes a array with a 1 for every spectrum, as the 
@@ -37,10 +40,16 @@ class loadData:
                         loglam = np.array(binHDU['loglam'],dtype=np.longdouble)
                         flux = np.array(binHDU['flux'],dtype=np.longdouble)
                         #TODO: Implement a dynamic maximum pad length system
-                        loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant', constant_values=0)
-                        flux = np.pad(flux, (0,5000 - len(flux)), mode='constant', constant_values=0)
+                        loglam = np.pad(loglam, (0, self._maxLength - len(loglam))
+                        , mode='constant', constant_values=0)
+
+                        flux = np.pad(flux, (0,self._maxLength - len(flux))
+                        , mode='constant', constant_values=0)
+
                         spectrum = np.stack([loglam,flux])
-                        self._qsoSpectra = np.append(self._qsoSpectra, [spectrum], axis=0)
+                        self._qsoSpectra = np.append(self._qsoSpectra
+                        ,[spectrum], axis=0)
+
             self._qsoLabels = np.ones(self._qsoSpectra.shape[0],dtype=np.int8)
         except FileNotFoundError:
             print("FileNotFoundError, please check if the path is valid")
@@ -48,7 +57,7 @@ class loadData:
             print(f"A error occured : {e}")
 
 
-    def loadOthData(self):
+    def _loadOthData(self):
         """
             This loads the relevant columns from the .fits files , which is basically 
             the spectrum, and then makes a array with a 0 for every spectrum, as the 
@@ -66,17 +75,23 @@ class loadData:
                         loglam = np.array(binHDU['loglam'],dtype=np.longdouble)
                         flux = np.array(binHDU['flux'],dtype=np.longdouble)
                         #TODO: Implement a dynamic maximum pad length system
-                        loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant', constant_values=0)
-                        flux = np.pad(flux, (0,5000 - len(flux)), mode='constant', constant_values=0)
+                        loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant'
+                        , constant_values=0)
+
+                        flux = np.pad(flux, (0,5000 - len(flux)), mode='constant'
+                        , constant_values=0)
+
                         spectrum = np.stack([loglam,flux])
-                        self._othSpectra = np.append(self._othSpectra, [spectrum], axis=0)
+                        self._othSpectra = np.append(self._othSpectra, 
+                        [spectrum], axis=0)
+
             self._othLabels = np.zeros(self._othSpectra.shape[0],dtype=np.int8)
         except FileNotFoundError:
             print("FileNotFoundError, please check if the path is valid")
         except Exception as e:
             print(f"A error occured : {e}")
 
-    def combineDataLabels(self):
+    def _combineDataLabels(self):
         """
             This combines both the arrays of labels into one array, to be fed 
             into the neural network.
@@ -84,7 +99,7 @@ class loadData:
 
         self.labels = np.concatenate((self._qsoLabels,self._othLabels),axis=0)
 
-    def combineDataSpectra(self):
+    def _combineDataSpectra(self):
         """
             This combines both the arrays of labels into one array, to be fed 
             into the preProcessor class
@@ -99,9 +114,9 @@ class loadData:
         if self.spectra:
             return self.spectra
         else:
-            self.loadQSOData()
-            self.loadOthData()
-            self.combineDataSpectra()
+            self._loadQSOData()
+            self._loadOthData()
+            self._combineDataSpectra()
             return self.spectra
 
     @staticmethod
@@ -112,9 +127,9 @@ class loadData:
         if self.labels:
             return self.labels
         else:
-            self.loadQSOData()
-            self.loadOthData()
-            self.combineDataLabels()
+            self._loadQSOData()
+            self._loadOthData()
+            self._combineDataLabels()
             return self.labels
 
 
@@ -122,8 +137,50 @@ dataLoader = loadData()
 
 class preProcessor:
     def __init__(self,spectra=dataLoader.getSpectra):
-        self.spectra = spectra
+        self._spectra = spectra
         self.outSpectra = None
+        self._normalizedSpectra = None
+
+    def _normalize(self):
+        """
+            Just, well normalizes the data.
+        """
+
+        normalizeValue = np.linalg.norm(self._spectra, 'fro')
+        self._normalizedSpectra = self._spectra / normalizeValue
+    
+    def _randomShifts(self):
+        """
+            Add some very small random shifts to the spectra
+        """
+
+        shiftValues = np.array([-0.0001,-0.00009,-0.00008,-0.00007,-0.00006
+        ,-0.00005,-0.00004,-0.00003,-0.00002,-0.00001
+        ,0.00001, 0.00002,0.00003,0.00004,
+        0.00005,0.00006,0.00007,0.00008
+        ,0.00009,0.0001]
+        ,dtype=np.longdouble) 
+        numShifts = 24000 #just a random number
+        for _ in range(numShifts):
+            random2dSubarray = np.random.randint(0,self._normalizedSpectra.shape[0])
+            random1dSubarray = np.random.randint(0,self._normalizedSpectra.shape[1])
+            randomValue = np.random.randint(0,self._normalizedSpectra.shape[2])
+            shiftValue = np.random.choice(shiftValues)
+            self.outSpectra = np.copy(self._normalizedSpectra)
+            self.outSpectra[random2dSubarray][random1dSubarray][randomValue] += shiftValue
+
+        def getSpectra(self):
+            """
+                Just returns the preprocessed array
+            """
+
+            if self.outSpectra:
+                return self.outSpectra
+            else:
+                self._normalize()
+                self._randomShifts()
 
 
 
+
+    
