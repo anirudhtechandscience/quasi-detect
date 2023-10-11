4383056cf18a08fd5311e1c60cf342490e14d190
Your Name <anirudhm.techandscience@gmail.com>
2023-10-11 15:47:50 +0000

    refactor code according to feedback

--- backend/load_data.py
+++ backend/load_data.py
@@ -22,51 +22,17 @@ class loadData:
         self.spectra = None
         self._maxLength = 5000
         
-    def _loadQSOData(self):
+
+
+    def _loadAllData(self):
         """
             This loads the relevant columns from the .fits files , which is basically 
-            the spectrum, and then makes a array with a 1 for every spectrum, as the 
-            labels(Initially it will just tell quasars apart from other objects to 
-            reduce complexity, later it will detect all kinds of objects). Both arrays 
-            will be combined with the arrays of the stars and galaxies using 
-            the combineData methods.
+            the spectrum, and then makes a array with a 0 and 1 for every spectrum, 
+            as the labels(Initially it will just tell quasars apart from other objects 
+            to reduce complexity, later it will detect all kinds of objects). 
         """ 
         try:
-            for fileName in os.listdir(self._dirPathQuasar):
-                if fileName.endswith('.fits'):
-                    filePath = os.path.join(self._dirPathQuasar, fileName)
-                    with fs.open(filePath) as spec:
-                        binHDU = spec[1].data
-                        loglam = np.array(binHDU['loglam'],dtype=np.longdouble)
-                        flux = np.array(binHDU['flux'],dtype=np.longdouble)
-                        #TODO: Implement a dynamic maximum pad length system
-                        loglam = np.pad(loglam, (0, self._maxLength - len(loglam))
-                        , mode='constant', constant_values=0)
-
-                        flux = np.pad(flux, (0,self._maxLength - len(flux))
-                        , mode='constant', constant_values=0)
-
-                        spectrum = np.stack([loglam,flux])
-                        self._qsoSpectra = np.append(self._qsoSpectra
-                        ,[spectrum], axis=0)
-
-            self._qsoLabels = np.ones(self._qsoSpectra.shape[0],dtype=np.int8)
-        except FileNotFoundError:
-            print("FileNotFoundError, please check if the path is valid")
-        except Exception as e:
-            print(f"A error occured : {e}")
-
-
-    def _loadOthData(self):
-        """
-            This loads the relevant columns from the .fits files , which is basically 
-            the spectrum, and then makes a array with a 0 for every spectrum, as the 
-            labels(Initially it will just tell quasars apart from other objects to 
-            reduce complexity, later it will detect all kinds of objects). Both arrays 
-            will be combined with the arrays of the quasars using 
-            the combineData methods.
-        """ 
-        try:
+            print("Loading other data")
             for fileName in os.listdir(self._dirPathOther):
                 if fileName.endswith('.fits'):
                     filePath = os.path.join(self._dirPathOther, fileName)
@@ -84,60 +50,53 @@ class loadData:
                         spectrum = np.stack([loglam,flux])
                         self._othSpectra = np.append(self._othSpectra, 
                         [spectrum], axis=0)
+                        print(self._othSpectra.shape)
+                    
+            print("Loading quasar data")
+
+            for fileName in os.listdir(self._dirPathQuasar):
+                if fileName.endswith('.fits'):
+                    filePath = os.path.join(self._dirPathQuasar, fileName)
+                    with fs.open(filePath) as spec:
+                        binHDU = spec[1].data
+                        loglam = np.array(binHDU['loglam'],dtype=np.longdouble)
+                        flux = np.array(binHDU['flux'],dtype=np.longdouble)
+                        #TODO: Implement a dynamic maximum pad length system
+                        loglam = np.pad(loglam, (0, self._maxLength - len(loglam))
+                        , mode='constant', constant_values=0)
+
+                        flux = np.pad(flux, (0,self._maxLength - len(flux))
+                        , mode='constant', constant_values=0)
+
+                        spectrum = np.stack([loglam,flux])
+                        self._qsoSpectra = np.append(self._qsoSpectra
+                        ,[spectrum], axis=0)
+                        print(self._qsoSpectra.shape)
 
             self._othLabels = np.zeros(self._othSpectra.shape[0],dtype=np.int8)
+            self._qsoLabels = np.ones(self._qsoSpectra.shape[0],dtype=np.int8) 
+            self.spectra = np.concatenate((self._qsoSpectra,self._othSpectra),axis=0)
+            self.labels = np.concatenate((self._qsoLabels,self._othLabels),axis=0)
         except FileNotFoundError:
             print("FileNotFoundError, please check if the path is valid")
         except Exception as e:
             print(f"A error occured : {e}")
 
-    def _combineDataLabels(self):
+    def getData(self):
         """
-            This combines both the arrays of labels into one array, to be fed 
-            into the neural network.
-        """
-
-        self.labels = np.concatenate((self._qsoLabels,self._othLabels),axis=0)
-
-    def _combineDataSpectra(self):
-        """
-            This combines both the arrays of labels into one array, to be fed 
-            into the preProcessor class
-        """
-        self.spectra = np.concatenate((self._qsoSpectra,self._othSpectra),axis=0)
-
-    @staticmethod
-    def getSpectra(self):
-        """
-            A simple method to retrieve the spectra
-        """
-        if self.spectra:
-            return self.spectra
-        else:
-            self._loadQSOData()
-            self._loadOthData()
-            self._combineDataSpectra()
-            return self.spectra
-
-    @staticmethod
-    def getLabels(self):
-        """
-            A simple method to retrieve the labels
+            A simple method to retrieve the data
         """
         if self.labels:
             return self.labels
         else:
-            self._loadQSOData()
-            self._loadOthData()
-            self._combineDataLabels()
-            return self.labels
+            self._loadAllData()
+            return (self.spectra,self.labels)
 
-
-dataLoader = loadData()
 
 class preProcessor:
-    def __init__(self,spectra=dataLoader.getSpectra):
-        self._spectra = spectra
+    def __init__(self,spectra):
+        self._spectra = spectra[0]
+        self.labels = spectra[1]
         self.outSpectra = None
         self._normalizedSpectra = None
 
@@ -174,7 +133,7 @@ class preProcessor:
         ,0.00009,0.0001]
         ,dtype=np.longdouble) 
         self.outSpectra = np.copy(self._normalizedSpectra)
-        numShifts = self._normalizedSpectra.shape[0] 
+        numShifts = self._normalizedSpectra.shape[0] * 0.75
         numSubShift = 450 #every sub array will have this amount of values shifted
         self.outSpectra = np.copy(self._normalizedSpectra)
         for _ in range(numShifts):
@@ -187,7 +146,7 @@ class preProcessor:
                 twoDSubarray[random1dSubarray][randomValue] += shiftValue
             self.outSpectra = np.append(self.outSpectra,[twoDSubarray],axis=0)
 
-        def getSpectra(self):
+        def getData(self):
             """
                 Just returns the preprocessed array
             """
@@ -197,4 +156,4 @@ class preProcessor:
             else:
                 self._normalize()
                 self._randomShifts()
-                return self.outSpectra
+                return (self.outSpectra,self.labels)
---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from load_data import loadData, preProcessor
+import numpy as np
+
+#simple script to run all the other scipts
+
+dataLoader = loadData()
+data = dataLoader.getData()
+
+process = preProcessor(spectra=data)
+preProcessedData = process.getData()
+preprocessedSpec = preProcessedData[0]
+print(preProcessedData[2])
+print(np.min(preProcessedData))
+print(np.max(preProcessedData))
+
+np.savetxt("features.csv",preProcessedData[0],delimiter=",")
+np.savetxt("labels.csv",preProcessedData[1],delimiter=",")
