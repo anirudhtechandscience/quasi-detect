baaac37355a26f105cffacf6f41ae610f27a645f
Your Name <anirudhm.techandscience@gmail.com>
2023-10-22 04:11:43 +0000

    remove the code my computer cant handle, also add a cap so it stops freezing up

--- backend/load_data.py
+++ backend/load_data.py
@@ -21,16 +21,21 @@ class loadData:
         othLabels = np.zeros(othSpectra.shape[0], dtype=np.int8)
         self.labels = np.concatenate((qsoLabels, othLabels), axis=0)
 
-
     def _readSpectra(self, path):
-        spectra = np.empty((0, 2, 5000), dtype=np.longdouble)        
+        spectra = np.empty((0, 2, 5000), dtype=np.float32)
+        i = 0
         for fileName in os.listdir(path):
+            i += 1
+            if i == 35000:
+                break
+            #its just a cap , because my computer freeze up when i try more
+
             print(spectra.shape)
             if fileName.endswith('.fits'):
-                filePath= os.path.join(path, fileName)
+                filePath = os.path.join(path, fileName)
                 spectrum = self._getSpectrum(filePath)
                 try:
-                    spectra = np.append(spectra, [spectrum],axis=0)
+                    spectra = np.append(spectra, [spectrum], axis=0)
                     logging.debug(f"Spectra shape:{spectra.shape}")
                 except FileNotFoundError:
                     logging.critical("FileNotFoundError")
@@ -40,17 +45,18 @@ class loadData:
         time.sleep(15)
         return spectra
 
-    def _getSpectrum(self, filePath):
+    @staticmethod
+    def _getSpectrum(filePath):
         """ returns the spectrum from a .fits file """
 
         with fs.open(filePath) as spec:
             binHDU = spec[1].data
-            loglam = np.array(binHDU['loglam'], dtype=np.longdouble)
-            flux = np.array(binHDU['flux'], dtype=np.longdouble)
+            loglam = np.array(binHDU['loglam'], dtype=np.float32)
+            flux = np.array(binHDU['flux'], dtype=np.float32)
 
             #TODO: Implement a dynamic maximum pad length system
-            loglam = np.pad(loglam,(0, 5000 - len(loglam)), mode='constant', constant_values=0)
-            flux = np.pad(flux, (0,5000 - len(flux)), mode='constant', constant_values=0)
+            loglam = np.pad(loglam, (0, 5000 - len(loglam)), mode='constant', constant_values=0)
+            flux = np.pad(flux, (0, 5000 - len(flux)), mode='constant', constant_values=0)
 
             spectrum = np.stack([loglam, flux])     
         return spectrum
@@ -62,32 +68,27 @@ class loadData:
         """
             A simple method to retrieve the data
         """
-        return (self.spectra,self.labels)
+        return (self.spectra, self.labels)
 
 
 class preProcessor:
-    def __init__(self,spectra):
+    def __init__(self, spectra):
         self._spectra = spectra[0]
         self._labels = spectra[1]
-        self._outSpectra = None
+        self.outSpectra = None
         self._normalizedSpectra = None
-        self._testFeatures = None
-        self._testLabels = None
-        self._trainFeatures = None
-        self._trainLabels = None
-        self.trainData = None
-        self.testData = None
+
 
     def _normalize(self):
         """
             Just, well normalizes the data between 0 and 1.
             """
-        loglam = np.array(dtype=np.longdouble)
-        flux = np.array(dtype=np.longdouble)
+        loglam = np.empty((0, 5000), dtype=np.float32)
+        flux = np.empty((0, 5000), dtype=np.float32)
         for _ in range(self._spectra.shape[0]):
-            loglam = np.stack([loglam,self._spectra[_][0]])
+            loglam = np.stack([loglam, self._spectra[_][0]])
         for __ in range(self._spectra.shape[0]):
-            flux = np.stack([flux,self._spectra[__][1]])
+            flux = np.stack([flux, self._spectra[__][1]])
         normalizeFlux = np.linalg.norm(flux, 'fro')
         normalizeLoglam = np.linalg.norm(loglam, 'fro')
         self._normalizedSpectra = np.copy(self._spectra)
@@ -99,68 +100,16 @@ class preProcessor:
 
 
     
-    def _randomShifts(self):
-        """
-            Add some very small random shifts to the spectra,to simulate noise, 
-            and also to augment the data.
-        """
-
-        shiftValues = np.array([-0.0001,-0.00009,-0.00008,-0.00007,-0.00006
-        ,-0.00005,-0.00004,-0.00003,-0.00002,-0.00001
-        ,0.00001, 0.00002,0.00003,0.00004,
-        0.00005,0.00006,0.00007,0.00008
-        ,0.00009,0.0001]
-        ,dtype=np.longdouble) 
-        self._outSpectra = np.copy(self._normalizedSpectra)
-        numShifts = self._normalizedSpectra.shape[0] * 0.75
-        numSubShift = 450 #every sub array will have this amount of values shifted
-        self._outSpectra = np.copy(self._normalizedSpectra)
-        for _ in range(numShifts):
-            random2dSubarray = np.random.randint(0,self._normalizedSpectra.shape[0]) 
-            twoDSubarray = np.copy(self._normalizedSpectra[random2dSubarray])
-            for ___ in range(numSubShift):
-                random1dSubarray = np.random.randint(0,self._normalizedSpectra.shape[1])
-                randomValue = np.random.randint(0,self._normalizedSpectra.shape[2])
-                shiftValue = np.random.choice(shiftValues)
-                twoDSubarray[random1dSubarray][randomValue] += shiftValue
-            self._outSpectra = np.append(self._outSpectra,[twoDSubarray],axis=0)
-        
-    def _trainTestSplit(self):
-        last15Percent = int(self._outSpectra.shape[0] * 0.85)
-        self._testFeatures = self._outSpectra[last15Percent:]
-        self._testLabels = self._labels[last15Percent:]
-        self._trainFeatures = self._outSpectra[:last15Percent]
-        self._trainLabels = self._labels[:last15Percent]
-    
     def _convToTensorflowObject(self):
-        self.trainData = tf.data.Dataset.from_tensor_slices((self._trainFeatures,self._trainLabels))
-        self.testData = tf.data.Dataset.from_tensor_slices((self._testFeatures,self._testLabels))
-        batchSize = 1024
-        bufferSize = 100
-
-        self.trainData = self.trainData.shuffle(bufferSize).batch(batchSize)
-        self.testData = self.testData.batch(batchSize)
+        self.outSpectra = tf.data.Dataset.from_tensor_slices((self._normalizedSpectra, self._labels))
 
 
-
-    def getTrainData(self):
-        if self.trainData:
-            return self.trainData
+    def getData(self):
+        if self.outSpectra:
+            return self.outSpectra
         else:
             self._normalize()
-            self._randomShifts()
-            self._trainTestSplit()
             self._convToTensorflowObject()
-            return self.trainData()
-
-    def getTestData(self):
-        if self.testData:
-            return self.testData
-        else:
-            self._normalize()
-            self._randomShifts()
-            self._trainTestSplit()
-            self._convToTensorflowObject()
-            return self.testData()
+            return self.outSpectra()
 
 
--- backend/nn.py
+++ backend/nn.py
@@ -9,3 +9,5 @@ class Train:
     def __init__(self, traindata, testdata):
         self.train = traindata
         self.test = testdata
+
+    def neural_network(self):
