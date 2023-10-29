09a45622e7fbb710a2b2f278d5bfad86b17f9094
Your Name <anirudhm.techandscience@gmail.com>
2023-10-29 15:04:47 +0000

    add requirements.txt and ive begun writing tests

--- backend/testload_data.py
+++ backend/testload_data.py
@@ -1,4 +1,15 @@
-import pytest
+from load_data import loadData, preProcessor
+ld = loadData(qsoPath="test/testQSO", othPath="test/testoth")
+
+
+class testloadData:
+    @staticmethod
+    def testShape():
+        assert ld.spectra.shape == (8, 2, 5000)
+
+    @staticmethod
+    def test():
+        pass
 
 
 
