2c9b695c877a35e9cbaa0c4c731c9e2e024d08e7
Your Name <anirudhm.techandscience@gmail.com>
2023-10-29 16:09:46 +0000

    got the tests

--- backend/testload_data.py
+++ backend/testload_data.py
@@ -2,14 +2,16 @@ from load_data import loadData, preProcessor
 ld = loadData(qsoPath="test/testQSO", othPath="test/testoth")
 
 
-class testloadData:
-    @staticmethod
-    def testShape():
-        assert ld.spectra.shape == (8, 2, 5000)
 
-    @staticmethod
-    def test():
-        pass
+def testShape():
+    assert ld.spectra.shape == (8, 2, 5000)
 
+
+def testLabels():
+    assert ld.labels[1] == 1
+    assert ld.labels[-1] == 0
+    assert ld.labels.shape == (8,)
+
+def
 
 
